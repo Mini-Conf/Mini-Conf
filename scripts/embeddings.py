a12ecf192e918b470af0cf461da5b83c512381df
Alex Rush <sasha.rush@gmail.com>
2020-05-17 15:56:50 -0400

    Make embeddings

--- scripts/embeddings.py
+++ scripts/embeddings.py
@@ -1,6 +1,6 @@
 import csv, argparse
-import transformers
-
+import transformers, torch
+import gc
 def parse_arguments():
     parser = argparse.ArgumentParser(description="MiniConf Portal Command Line")
 
@@ -15,7 +15,17 @@ if __name__ == "__main__":
     tokenizer = transformers.AutoTokenizer.from_pretrained("deepset/sentence_bert")
 
     model = transformers.AutoModel.from_pretrained("deepset/sentence_bert")
+    model.eval()
+
     
     with open(args.papers, "r") as f:
-        for row in list(csv.DictReader(f)):
-            print(row["abstract"])
+        abstracts = list(csv.DictReader(f))
+        all_abstracts = torch.zeros(len(abstracts), 768)
+        with torch.no_grad():
+            for i, row in enumerate(abstracts):
+
+                input_ids = torch.tensor([tokenizer.encode(row["abstract"])][:512])
+                all_hidden_states, _ = model(input_ids)[-2:]
+                all_abstracts[i] = all_hidden_states.mean(0).mean(0)
+                print(i)
+    torch.save(all_abstracts, "embeddings.torch")
---
+++ scripts/reduce.py
@@ -0,0 +1,28 @@
+import torch
+import numpy
+import csv
+import sklearn.manifold
+import argparse
+import json
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="MiniConf Portal Command Line")
+    parser.add_argument('papers', default=False,
+                        help="paper file")
+
+    parser.add_argument('embeddings', default=False,
+                        help="embeddings file to shrink")
+    
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    emb = torch.load(args.embeddings)
+    out = sklearn.manifold.TSNE(n_components=2).fit_transform(emb.numpy())
+    d = []
+    with open(args.papers, "r") as f:
+        abstracts = list(csv.DictReader(f))
+        for i, row in enumerate(abstracts):
+            d.append({"id": row["UID"], "pos" : out[i].tolist()})
+    print(json.dumps(d))
