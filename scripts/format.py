1a606d27b8954fb32bbabe18f46b9517bd8d1811
animesh-007 <animeshagarwal7@gmail.com>
2020-05-23 15:22:11 +0530

    style check on file

--- scripts/format.py
+++ scripts/format.py
@@ -6,17 +6,17 @@ for f in glob.glob(sys.argv[1]):
     outfile = f + ".html"
     out = open(outfile, "w")
     for l in open(f):
-        if l.strip().startswith("{{")  or l.strip().startswith("{%"):
+        if l.strip().startswith("{{") or l.strip().startswith("{%"):
             for j in range(len(l)):
                 if l[j] != " ":
                     break
 
-            print(" "* j + "<!-- prettier-ignore -->", file=out)
+            print(" " * j + "<!-- prettier-ignore -->", file=out)
 
         print(l, end="", file=out)
     out.close()
-    os.system("prettier %s  --write"%outfile)
-    
+    os.system("prettier %s  --write" % outfile)
+
     orig = open(f, "w")
     for l in open(outfile):
         if l.strip().startswith("<!-- prettier-ignore -->"):
@@ -25,4 +25,4 @@ for f in glob.glob(sys.argv[1]):
             print(l, end="", file=orig)
 
     orig.close()
-    os.system("rm %s"%outfile)
+    os.system("rm %s" % outfile)
