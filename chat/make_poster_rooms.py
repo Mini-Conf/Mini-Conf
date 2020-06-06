a84e1ef93fd2f1ec27e2223a1016a6e912e290d5
Hao Fang <haofang1990@gmail.com>
2020-06-05 20:57:51 -0700

    format

--- chat/make_poster_rooms.py
+++ chat/make_poster_rooms.py
@@ -1,51 +1,44 @@
 import argparse
 import csv
-import random
-import string
 
-from requests import sessions
-from pprint import pprint
-from rocketchat_API.rocketchat import RocketChat
 import yaml
+from requests import sessions
+from rocketchat_API.rocketchat import RocketChat
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="MiniConf Portal Command Line")
     parser.add_argument("--config", default="config.yml", help="Configuration yaml")
     parser.add_argument("--papers", default="../sitedata/papers.csv", help="Papers CSV")
-    parser.add_argument("--test", action='store_true')
+    parser.add_argument("--test", action="store_true")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
 
-
     config = yaml.load(open(args.config))
     papers = list(csv.DictReader(open(args.papers)))
 
     with sessions.Session() as session:
-        rocket = RocketChat(config["username"],
-                            config["password"],
-                            server_url=config["server"],
-                            session=session)
+        rocket = RocketChat(
+            config["username"],
+            config["password"],
+            server_url=config["server"],
+            session=session,
+        )
 
         for paper in papers:
             channel_name = "paper_" + paper["UID"]
             if not args.test:
                 created = rocket.channels_create(channel_name).json()
                 print(channel_name, created)
-            id = rocket.channels_info(channel = channel_name).json()["channel"]["_id"]
+            channel_info = rocket.channels_info(channel=channel_name).json()
+            room_id = channel_info["channel"]["_id"]
 
-            
-            # Change to topic of papers. 
-            topic = "%s - %s"%(
-                paper["title"],
-                paper["authors"],
-            )
+            # Change to topic of papers.
+            topic = "%s - %s" % (paper["title"], paper["authors"],)
             if not args.test:
-                rocket.channels_set_topic(id, topic).json()
+                rocket.channels_set_topic(room_id, topic).json()
 
             print("Creating " + channel_name + " topic " + topic)
-
-
-
