0b2a38fe55e1166fe1d59f5396ec3d2535580647
Tom Pollard <tpollard@mit.edu>
2020-06-08 16:49:12 -0400

    add new paper location

--- static/js/papers.js
+++ static/js/papers.js
@@ -201,7 +201,7 @@ const keyword = kw => `<a href="papers.html?filter=keywords&search=${kw}"
                        class="text-secondary text-decoration-none">${kw.toLowerCase()}</a>`
 
 const card_image = (openreview, show) => {
-    if (show) return ` <center><img class="lazy-load-img cards_img" data-src="https://iclr.github.io/iclr-images/small/${openreview.id}.jpg" width="80%"/></center>`
+    if (show) return ` <center><img class="lazy-load-img cards_img" data-src="static/images/papers/${openreview.id}.png" width="80%"/></center>`
     else return ''
 }
 
