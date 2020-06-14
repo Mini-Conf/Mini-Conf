95d7c70ab31e9b2c2b23eb24e0a7a193d32492d6
Guy Aglionby <GuyAglionby@users.noreply.github.com>
2020-06-14 14:12:14 +0100

    Add demo link (#32) (#69)
    * Add demo link (#32)
    
    * linting fix
    
    * Undo linting fix
    
    * linting fix attempt
    
    * Linting fix
    
    * update sitedate_acl2020
    
    Co-authored-by: Hao Fang <haofang1990@gmail.com>

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main(site_data_path):
         elif typ == "yml":
             site_data[name] = yaml.load(open(f).read(), Loader=yaml.SafeLoader)
 
-    for typ in ["papers", "speakers", "workshops"]:
+    for typ in ["papers", "speakers", "workshops", "demos"]:
         by_uid[typ] = {}
         for p in site_data[typ]:
             by_uid[typ][p["UID"]] = p
@@ -160,6 +160,7 @@ def format_paper(v):
             "abstract": v["abstract"],
             "TLDR": v["abstract"],
             "pdf_url": v.get("pdf_url", ""),
+            "demo_url": by_uid["demos"].get(v["UID"], {}).get("demo_url", ""),
             "track": v.get("track", ""),
             # TODO: Fill this info in `main(sitedata)` using an external file.
             "sessions": [
